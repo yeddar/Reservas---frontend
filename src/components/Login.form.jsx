c8d38515b1006824a800a50c1558e68c991f7f07
Diego B <diego-995@hotmail.com>
2025-01-19 20:35:56 +0100

    Corregir bug validación credenciales incorrectas

--- src/components/Login.form.jsx
+++ src/components/Login.form.jsx
@@ -47,7 +47,7 @@ const LoginForm = () => {
         setError(response.message || "Error al iniciar sesión");
       }
     } catch (err) {
-      setError("Error en el servidor.");
+      setError(err.message || "Error al iniciar sesión");
     }
     finally {
       setLoading(false); 
